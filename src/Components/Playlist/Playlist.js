925e9e7927cdd910c07b31c24b4423b54227bbb8
Matpdev <matheus2ep@gmail.com>
2022-06-12 19:20:09 -0300

    Changing to react hooks

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,33 +1,38 @@
-import React from 'react'
-import "./Playlist.css"
+import React from "react";
+import "./Playlist.css";
 
-import TrackList from '../TrackList/TrackList';
+import TrackList from "../TrackList/TrackList";
 
 class Playlist extends React.Component {
-   constructor(props){
-      super(props);
+  constructor(props) {
+    super(props);
 
-      this.handleNameChange = this.handleNameChange.bind(this);
-   }
-   
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
 
-   handleNameChange(event){
-      this.props.onNameChange(event.target.value);
-   }
+  handleNameChange(event) {
+    this.props.onNameChange(event.target.value);
+  }
 
-   
-   render() {
-      return (
-         <div className="Playlist">
-            <input onChange={this.handleNameChange} defaultValue={'New Playlist'}></input>
-            <TrackList track={this.props.PlaylistTracks}
-               isRemoval={true}
-               onRemove={this.props.onRemove}/>
+  render() {
+    return (
+      <div className="Playlist">
+        <input
+          onChange={this.handleNameChange}
+          defaultValue={"New Playlist"}
+        ></input>
+        {/* <TrackList
+          track={this.props.PlaylistTracks}
+          isRemoval={true}
+          onRemove={this.props.onRemove}
+        /> */}
 
-               <button className="Playlist-save" onClick={this.props.onSave}>Save to Spotify</button>
-         </div>
-      );
-   }
+        <button className="Playlist-save" onClick={this.props.onSave}>
+          Save to Spotify
+        </button>
+      </div>
+    );
+  }
 }
 
-export default Playlist
+export default Playlist;
--- src/Components/SeachBar/SearchBar.js
+++ src/Components/SeachBar/SearchBar.js
@@ -1,41 +1,22 @@
-import React, { Component } from 'react'
-import './SearchBar.css'
+import React, { useState } from "react";
+import "./SearchBar.css";
 
-export default class SearchBar extends Component {
+const SearchBar = ({ search }) => {
+  const [term, setTerm] = useState("");
 
-   constructor(props){
-      super(props);
-   
-      this.state={
-         term:""
-      };
+  return (
+    <div className="SearchBar">
+      <input
+        placeholder="Enter Song, album or artist"
+        onChange={(e) => setTerm(e.target.value)}
+        onKeyUp={term}
+      ></input>
+      <button className="SearchButton" onClick={search}>
+        {" "}
+        Search{" "}
+      </button>
+    </div>
+  );
+};
 
-      this.handleTerm=this.handleTermChange.bind(this);
-      this.search=this.search.bind(this);
-      this.handeEnter = this.handeEnter.bind(this);
-
-   }
-
-   handleTermChange(event){
-      this.setState({term: event.target.value});
-   }
-
-   search(){
-      this.props.onSearch(this.state.term);
-   }
-
-   handeEnter(event){
-      if(event.keycode === 13){
-         this.search();
-      }
-   }
-
-   render() {
-      return (
-         <div className="SearchBar">
-            <input placeholder="Enter Song, album or artist" onChange={this.handleTermChange} onKeyUp={this.handeEnter}></input>
-            <button className="SearchButton" onClick={this.search}> Search </button>
-         </div>
-      )
-   }
-}
+export default SearchBar;
